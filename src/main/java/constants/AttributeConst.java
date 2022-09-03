998238ffd73f8cf64f08bfd5fe5daf8b9ef1c0f6
kosuke.morino <tabacco_sunburst_0706@yahoo.co.jp>
2022-09-04 02:36:35 +0900

    定数ファイルを作成2

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -35,15 +35,15 @@ public enum AttributeConst {
     EMP_NAME("name"),
     EMP_ADMIN_FLG("admin_flag"),
 
-    //管理者フラグ
+    // 管理者フラグ
     ROLE_ADMIN(1),
     ROLE_GENERAL(0),
 
-    //削除フラグ
+    // 削除フラグ
     DEL_FLAG_TRUE(1),
     DEL_FLAG_FALSE(0),
 
-    //日報管理
+    // 日報管理
     REPORT("report"),
     REPORTS("reports"),
     REP_COUNT("reports_count"),
